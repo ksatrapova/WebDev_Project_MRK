6b46cc46116de625c3c15e34534dc3a1c5c659f8
richytaiwo <C24375003@myTUDublin.ie>
2024-12-02 12:14:54 +0000

    contact form
    reworked

---
+++ javascript/contact.js
@@ -0,0 +1,27 @@
+function checkInputs() {
+    const items = document.querySelectorAll(".item");
+  
+    for (const item of items) {
+      if (item.value === "") {
+        item.classList.add("error");
+        item.parentElement.classList.add("error");   
+  
+      }
+  
+      item.addEventListener("keyup", () => {
+        if (item.value !== "") {
+          item.classList.remove("error");
+          item.parentElement.classList.remove("error");
+        } else {
+          item.classList.add("error");
+          item.parentElement.classList.add("error");   
+  
+        }
+      });
+    }
+  }
+
+  form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // sendEmail();
+});
